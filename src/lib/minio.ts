b33160cb4029767bc81381eccc6b4c51484033fb
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-06-23 15:31:39 +0800

    refactor minio to ts

--- src/lib/minio.ts
+++ src/lib/minio.ts
@@ -4,8 +4,8 @@ const minioClient = new Client({
   endPoint: "localhost",
   port: 9000,
   useSSL: false,
-  accessKey: process.env.MINIO_ACCESS_KEY,
-  secretKey: process.env.MINIO_SECRET_KEY,
+  accessKey: process.env.MINIO_ACCESS_KEY || "minioadmin",
+  secretKey: process.env.MINIO_SECRET_KEY || "miniosecret",
 });
 
 export default minioClient;
