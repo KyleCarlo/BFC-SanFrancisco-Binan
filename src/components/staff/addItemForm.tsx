f32c0147df83a19cc5b6fa365a739a60b9513681
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-06-24 02:23:52 +0800

    deleted add item form

--- src/components/staff/addItemForm.tsx
+++
@@ -1,9 +0,0 @@
-import { ItemType } from "@models/ItemType";
-
-export default function AddItemForm({ itemType }: { itemType: ItemType }) {
-  if (itemType == ItemType.Food) {
-    return <div>food</div>;
-  } else if (itemType == ItemType.Beverage) {
-    return <div>beverage</div>;
-  }
-}
