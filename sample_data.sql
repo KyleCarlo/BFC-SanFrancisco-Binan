f7a7822dc9989e7e47bc4d548829bee907e131e9
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-06-24 01:52:05 +0800

    update sample data

--- sample_data.sql
+++ sample_data.sql
@@ -13,11 +13,11 @@ VALUES (1, 'Bake Mac', '/sample', 'Pasta', 'Popular', 'A classic Italian pasta d
 
 INSERT INTO `BeverageVariation` (`id`, `beverage_id`, `price`, `serving`, `concentrate`, `hot_cold`)
 VALUES (1, 1, 65, '12 oz', false, 'Hot'),
-       (2, 1, 70, '16 oz', false, 'Hot'),
+       (2, 1, 70, '16 oz', false, 'Cold'),
        (3, 1, 110, '500 ml', true, null),
        (4, 1, 210, '1 L', true, null),
        (5, 2, 65, '12 oz', false, 'Hot'),
-       (6, 2, 70, '16 oz', false, 'Hot'),
+       (6, 2, 70, '16 oz', false, 'Cold'),
        (7, 2, 110, '500 ml', true, null),
        (8, 2, 210, '1 L', true, null);
 
