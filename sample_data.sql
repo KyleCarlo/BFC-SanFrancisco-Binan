ec2532b6f0bf919d051572722ac5290618d43c79
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-06-24 23:25:01 +0800

    updated sample data

--- sample_data.sql
+++ sample_data.sql
@@ -11,17 +11,17 @@ INSERT INTO `Food` (`id`, `name`, `image`, `category`, `feature`, `description`)
 VALUES (1, 'Bake Mac', '/sample', 'Pasta', 'Popular', 'A classic Italian pasta dish made with macaroni and cheese.'),
        (2, 'Biscoff', '/sample', 'Cake', 'None', 'A delicious cake made with Biscoff cookies and cream cheese.');
 
-INSERT INTO `BeverageVariation` (`id`, `beverage_id`, `price`, `serving`, `concentrate`, `hot_cold`)
-VALUES (1, 1, 65, '12 oz', false, 'Hot'),
-       (2, 1, 70, '16 oz', false, 'Cold'),
-       (3, 1, 110, '500 ml', true, null),
-       (4, 1, 210, '1 L', true, null),
-       (5, 2, 65, '12 oz', false, 'Hot'),
-       (6, 2, 70, '16 oz', false, 'Cold'),
-       (7, 2, 110, '500 ml', true, null),
-       (8, 2, 210, '1 L', true, null);
+INSERT INTO `BeverageVariation` (`id`, `beverage_id`, `price`, `serving`, `concentrate`, `hot_cold`, `available`)
+VALUES (1, 1, 65, '12 oz', false, 'Hot', true),
+       (2, 1, 70, '16 oz', false, 'Cold', true),
+       (3, 1, 110, '500 ml', true, null, true),
+       (4, 1, 210, '1 L', true, null, true),
+       (5, 2, 65, '12 oz', false, 'Hot', true),
+       (6, 2, 70, '16 oz', false, 'Cold', true),
+       (7, 2, 110, '500 ml', true, null, true),
+       (8, 2, 210, '1 L', true, null, true);
 
-INSERT INTO `FoodVariation` (`id`, `food_id`, `price`, `serving`)
-VALUES (1, 1, 180, '1 Tub'),
-       (2, 2, 175, '1 Slice'),
-       (3, 2, 350, '2 Slices');
+INSERT INTO `FoodVariation` (`id`, `food_id`, `price`, `serving`, `available`)
+VALUES (1, 1, 180, '1 Tub', true),
+       (2, 2, 175, '1 Slice', true),
+       (3, 2, 350, '2 Slices', true);
