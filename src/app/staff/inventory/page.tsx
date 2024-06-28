0251226a0560813cd4d67e87c24f2e4046d0bf01
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-06-28 17:39:18 +0800

    client side fetch for menu items

--- src/app/staff/inventory/page.tsx
+++ src/app/staff/inventory/page.tsx
@@ -1,25 +1,26 @@
-import InventoryTable from "@components/staff/inventory/data-table";
-import beverageColumns from "@components/staff/inventory/beverage-columns";
-import foodColumns from "@components/staff/inventory/food-columns";
-import { ItemType } from "@models/Menu";
-import SelectItemType from "@components/staff/select-table";
+"use client";
+
+import { FoodTable, BeverageTable } from "@components/staff/inventory/table";
+import SelectItemType from "@components/staff/inventory/select-table";
 import { Input } from "@components/ui/input";
 import { Button } from "@components/ui/button";
 
-async function getMenuItems(itemType: ItemType) {
-  const response = await fetch(
-    `http://localhost:3000/api/menu?itemType=${itemType}`,
-    {
-      method: "GET",
-      cache: "no-store",
-    }
-  );
-  const { items } = await response.json();
-  return items;
-}
+import { ItemType } from "@models/Menu";
+import { useState, useEffect } from "react";
+import { Food } from "@models/Menu/Food";
+import { Beverage } from "@models/Menu/Beverage";
 
-export default async function InventoryPage() {
-  const items = await getMenuItems("food");
+import getMenuItems from "@hooks/getMenuItems";
+
+export default function InventoryPage() {
+  const [itemType, setItemType] = useState<ItemType>("beverage");
+  const [items, setItems] = useState<Food[] | Beverage[] | []>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    getMenuItems(itemType, setLoading, setItems);
+  }, [itemType]);
+
   return (
     <main className="flex flex-col justify-center items-center">
       <div className="flex items-center gap-4 p-4">
@@ -27,9 +28,13 @@ export default async function InventoryPage() {
           Add Item
         </Button>
         <Input placeholder="Search for Item Name" />
-        <SelectItemType current_selected="food" />
+        <SelectItemType current_selected={itemType} setItemType={setItemType} />
       </div>
-      <InventoryTable columns={foodColumns} data={items} />
+      {/* {loading && <p>Loading...</p>} */}
+      {!loading && itemType === "food" && <FoodTable items={items as Food[]} />}
+      {!loading && itemType === "beverage" && (
+        <BeverageTable items={items as Beverage[]} />
+      )}
     </main>
   );
 }
