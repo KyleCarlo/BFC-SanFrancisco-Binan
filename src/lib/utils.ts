1b3ea47172d041a755c63502ac4c2e0e6c863140
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-06-30 15:16:30 +0800

    refactor validation in item types

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { ItemTypeModel } from "../models/Menu";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -7,4 +8,10 @@ export function cn(...inputs: ClassValue[]) {
 
 export function capitalize(word: string) {
   return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+export function validateItemType(itemType: string) {
+  const properItemTypes = Object.keys(ItemTypeModel.Values);
+
+  return properItemTypes.includes(itemType);
 }
