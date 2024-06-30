4cceb6aa58f2a88b7fd50851362ee534be807468
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-06-30 15:15:38 +0800

    added image bucket in meta

--- src/app/api/image/route.ts
+++ src/app/api/image/route.ts
@@ -4,8 +4,8 @@ import minioClient from "@/src/lib/minio";
 export async function POST(req: NextRequest) {
   const data = await req.formData();
   const image = data.get("image") as File;
+  const bucket = data.get("bucket") as string;
   const buffer = Buffer.from(await image.arrayBuffer());
-  const bucket = "hello";
   const exists = await minioClient.bucketExists(bucket);
   if (!exists) {
     await minioClient.makeBucket(bucket, "us-east-1");
