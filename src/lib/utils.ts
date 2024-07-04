9f6da7e175db43d42ab714bfd17386bddcec0bbb
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-07-05 01:08:44 +0800

    changed validation of variations; added default value parsing

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,10 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { ItemType, ItemTypeModel } from "@models/Menu";
+import { Food } from "@models/Menu/Food";
+import { Beverage } from "@models/Menu/Beverage";
 import { BeverageVariation } from "@models/Menu/Beverage";
+import { DefaultValues } from "react-hook-form";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -26,6 +29,7 @@ export function validateVariation(
     message: "",
   };
   variations.forEach((variation) => {
+    console.log(variation.concentrate, variation.hot_cold);
     if (itemType === "beverage") {
       if (variation.concentrate && variation.hot_cold !== undefined) {
         response.isValid = false;
@@ -40,15 +44,45 @@ export function validateVariation(
 
     if (variation.serving === "") {
       response.isValid = false;
-      response.message = "Serving is required";
+      response.message = "Serving Size is Required.";
       return;
     }
 
     if (variation.price === "") {
       response.isValid = false;
-      response.message = "Price is required";
+      response.message = "Price is Required.";
       return;
     }
   });
   return response;
+}
+
+export function parseDefaultValues(defaultValues: Food | Beverage) {
+  let base = (defaultValues as Beverage).base;
+  let category = (defaultValues as Food).category;
+  if (base) base = (base as string).toLowerCase() as Beverage["base"];
+  if (category)
+    category = (category as string).toLowerCase() as Food["category"];
+
+  const parsed_variations = defaultValues.variations.map((variation) => {
+    const hot_cold = (variation as BeverageVariation).hot_cold;
+    const concentrate = (variation as BeverageVariation).concentrate;
+
+    return {
+      hot_cold: hot_cold ? hot_cold.toLowerCase() : "none",
+      concentrate: concentrate ? "true" : "false",
+      serving: variation.serving,
+      price: variation.price.toString(),
+      available: Number(variation.available) == 1 ? true : false,
+    };
+  });
+
+  return {
+    name: defaultValues.name,
+    description: defaultValues.description,
+    feature: defaultValues.feature.toLowerCase() as Beverage["feature"],
+    base,
+    category,
+    variations: parsed_variations,
+  } as DefaultValues<Food | Beverage>;
 }
