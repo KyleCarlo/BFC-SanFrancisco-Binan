17f2d91a43c177c70b5184cd5124f49eb4a7e9d6
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-07-05 01:19:04 +0800

    added route for getting presignedURL

--- src/app/api/image/route.ts
+++ src/app/api/image/route.ts
@@ -5,12 +5,41 @@ export async function POST(req: NextRequest) {
   const data = await req.formData();
   const image = data.get("image") as File;
   const bucket = data.get("bucket") as string;
-  const buffer = Buffer.from(await image.arrayBuffer());
-  const exists = await minioClient.bucketExists(bucket);
-  if (!exists) {
-    await minioClient.makeBucket(bucket, "us-east-1");
-    console.log("Bucket " + bucket + ' created in "us-east-1".');
+  try {
+    const buffer = Buffer.from(await image.arrayBuffer());
+    const exists = await minioClient.bucketExists(bucket);
+    if (!exists) {
+      await minioClient.makeBucket(bucket, "us-east-1");
+      console.log("Bucket " + bucket + ' created in "us-east-1".');
+    }
+    minioClient.putObject(bucket, image.name, buffer);
+    return NextResponse.json({ success: true }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Error uploading image" },
+      { status: 500 }
+    );
   }
-  minioClient.putObject(bucket, image.name, buffer);
-  return NextResponse.json({ success: true }, { status: 200 });
+}
+
+export async function GET(req: NextRequest) {
+  const bucket = req.nextUrl.searchParams.get("bucket") as string;
+  const filename = req.nextUrl.searchParams.get("filename") as string;
+
+  try {
+    const presignedUrl = await minioClient.presignedGetObject(
+      bucket,
+      filename,
+      24 * 60 * 60
+    );
+    console.log(presignedUrl);
+    return NextResponse.json({ presignedUrl }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Error fetching images" },
+      { status: 500 }
+    );
+  }
 }
