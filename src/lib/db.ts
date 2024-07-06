d9dae491ac5b10a75d9f86e558787c41dce58cee
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-07-06 17:37:06 +0800

    changed environment variables

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,7 +3,13 @@ import { Kysely, MysqlDialect } from "kysely";
 import { DB } from "kysely-codegen";
 
 const dialect = new MysqlDialect({
-  pool: createPool(process.env.DATABASE_URL || ""),
+  pool: createPool({
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || "3306"),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  }),
 });
 
 const db = new Kysely<DB>({
