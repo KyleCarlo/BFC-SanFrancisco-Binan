dbd305c2a403f14884ab66bd89e278da951898b6
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-07-06 18:43:24 +0800

    Revert "changed environment variables"
    This reverts commit d9dae491ac5b10a75d9f86e558787c41dce58cee.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,13 +3,7 @@ import { Kysely, MysqlDialect } from "kysely";
 import { DB } from "kysely-codegen";
 
 const dialect = new MysqlDialect({
-  pool: createPool({
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || "3306"),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  }),
+  pool: createPool(process.env.DATABASE_URL || ""),
 });
 
 const db = new Kysely<DB>({
