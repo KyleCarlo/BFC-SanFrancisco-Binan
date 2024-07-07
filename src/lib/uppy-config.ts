67ffa105567730c88b2010405b0df49b538751cb
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-07-07 17:01:03 +0800

    refactored: changed to object

--- src/lib/uppy-config.ts
+++ src/lib/uppy-config.ts
@@ -2,27 +2,26 @@ import Uppy from "@uppy/core";
 import XHRUpload from "@uppy/xhr-upload";
 import ImageEditor from "@uppy/image-editor";
 
-export const staffUppy = () =>
-  new Uppy({
-    autoProceed: false,
-    restrictions: { maxNumberOfFiles: 1, allowedFileTypes: ["image/*"] },
+export const staffUppy = new Uppy({
+  autoProceed: false,
+  restrictions: { maxNumberOfFiles: 1, allowedFileTypes: ["image/*"] },
+})
+  .use(XHRUpload, {
+    endpoint: "/api/image",
+    method: "POST",
+    fieldName: "image",
+    formData: true,
   })
-    .use(XHRUpload, {
-      endpoint: "/api/image",
-      method: "POST",
-      fieldName: "image",
-      formData: true,
-    })
-    .use(ImageEditor, {
-      actions: {
-        revert: true,
-        rotate: true,
-        granularRotate: true,
-        flip: false,
-        zoomIn: true,
-        zoomOut: true,
-        cropSquare: true,
-        cropWidescreen: false,
-        cropWidescreenVertical: false,
-      },
-    });
+  .use(ImageEditor, {
+    actions: {
+      revert: true,
+      rotate: true,
+      granularRotate: true,
+      flip: false,
+      zoomIn: true,
+      zoomOut: true,
+      cropSquare: true,
+      cropWidescreen: false,
+      cropWidescreenVertical: false,
+    },
+  });
