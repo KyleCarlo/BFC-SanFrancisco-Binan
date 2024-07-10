7bfde26ca256330e0bf82612feb54e049a813683
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-07-11 01:44:38 +0800

    added add to cart form

---
+++ src/components/customer/addToCart/form-sheet.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@components/ui/sheet";
+import AddToCartForm from "./form";
+import { Beverage } from "@models/Menu/Beverage";
+import { Food } from "@models/Menu/Food";
+import { CartItem } from "@models/Cart";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function AddToCartSheet({
+  children,
+  item,
+  fullWidth,
+  defaultCartItem,
+  formType,
+}: {
+  children: React.ReactNode;
+  item: Beverage | Food;
+  fullWidth?: boolean;
+  defaultCartItem?: CartItem;
+  formType: "create" | "update";
+}) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger className={`${fullWidth == true ? "w-full" : ""}`}>
+        {children}
+      </SheetTrigger>
+      <SheetContent className="w-full">
+        <AddToCartForm
+          variations={item.variations}
+          itemID={item.id}
+          setOpen={setOpen}
+          defaultValues={defaultCartItem}
+          formType={formType}
+        >
+          <SheetHeader>
+            <div className="w-1/3 relative pt-[33%] left-1/3">
+              <Image
+                fill={true}
+                src={item.image}
+                alt={`Image of ${item.name}`}
+                className="w-full h-full top-1/3 left-1/3 object-cover rounded-md"
+              />
+            </div>
+            <SheetTitle className="text-gold">{item.name}</SheetTitle>
+            <SheetDescription className="text-justify">
+              {item.description}
+            </SheetDescription>
+          </SheetHeader>
+        </AddToCartForm>
+      </SheetContent>
+    </Sheet>
+  );
+}
