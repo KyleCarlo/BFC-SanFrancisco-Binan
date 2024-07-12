ce94bc3cb1e7bf5a98b2b09a8a5d8c0e7f395cce
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-07-13 01:16:10 +0800

    lists transferred to components

--- src/app/staff/orders/page.tsx
+++ src/app/staff/orders/page.tsx
@@ -1,7 +1,28 @@
+"use client";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@components/ui/tabs";
+import socket from "@lib/socket";
+
 export default function StaffOrdersPage() {
+  socket.on("orders", (data) => {
+    console.log(data);
+    alert(data);
+  });
   return (
-    <main>
-      <h1>Staff Orders</h1>
-    </main>
+    <div className="p-4">
+      <Tabs
+        defaultValue="incoming"
+        className="w-full flex flex-col items-center"
+      >
+        <TabsList>
+          <TabsTrigger value="incoming">Incoming</TabsTrigger>
+          <TabsTrigger value="processing">Processing</TabsTrigger>
+          <TabsTrigger value="completed">Completed</TabsTrigger>
+        </TabsList>
+        <TabsContent value="incoming">incoming orders here</TabsContent>
+        <TabsContent value="processing">processing orders here</TabsContent>
+        <TabsContent value="completed">completed</TabsContent>
+      </Tabs>
+    </div>
   );
 }
