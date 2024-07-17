c19b8639f8bdcd38c51e41f6a7a1425dcbdcbf16
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-07-17 20:10:19 +0800

    generalized Uppy config for staffs

--- src/lib/uppy-config.ts
+++ src/lib/uppy-config.ts
@@ -2,14 +2,14 @@ import Uppy from "@uppy/core";
 import XHRUpload from "@uppy/xhr-upload";
 import ImageEditor from "@uppy/image-editor";
 
-export const staffUppy = (formType: "create" | "update") =>
+export const staffUppy = (method: "GET" | "POST" | "PUT") =>
   new Uppy({
     autoProceed: false,
     restrictions: { maxNumberOfFiles: 1, allowedFileTypes: ["image/*"] },
   })
     .use(XHRUpload, {
-      endpoint: "/api/image",
-      method: formType === "create" ? "POST" : "PUT",
+      endpoint: `/api/image`,
+      method: method,
       fieldName: "image",
       formData: true,
     })
