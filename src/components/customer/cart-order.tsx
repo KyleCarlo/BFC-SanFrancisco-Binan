3989ef17b84a83d6efb790b83c226e84f5d570d3
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-07-17 20:11:43 +0800

    added order confirmation carousel

--- src/components/customer/cart-order.tsx
+++ src/components/customer/cart-order.tsx
@@ -2,6 +2,7 @@
 import { ItemDetailsList } from "@models/Cart";
 import { ScrollArea } from "@components/ui/scroll-area";
 import { Button } from "@components/ui/button";
+import OrderForm from "./orderForm";
 import CartList from "./cart-list";
 import verifyItemAvailability from "@hooks/verifyItemAvailability";
 import { useEffect, useState } from "react";
@@ -18,6 +19,7 @@ export default function CartOrderSubmission({
   const [validated_quantity, setValidatedQuantity] = useState(quantity);
   const [validated_total_cost, setValidatedTotalCost] = useState(total_cost);
   const [available_orders, setAvailableOrders] = useState<boolean[]>([]);
+  const [orderConfirmed, setOrderConfirmed] = useState(false);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -36,23 +38,39 @@ export default function CartOrderSubmission({
   return (
     <>
       <hr className="-mb-2" />
-      <ScrollArea className="px-3 -my-4">
-        <div className="flex flex-col">
-          {!loading ? (
-            itemDetailsList.map((item, index) => {
-              return (
-                <CartList
-                  key={item.id}
-                  item={item}
-                  available={available_orders[index]}
-                />
-              );
-            })
-          ) : (
-            <div>Loading...</div>
-          )}
+      <div className="-my-4 overflow-hidden flex justify-center relative">
+        <ScrollArea
+          className={`px-3 flex-grow h-full absolute  transition-all duration-500 ${
+            orderConfirmed ? "right-[100%]" : "right-[0%]"
+          }`}
+        >
+          <div className="flex flex-col">
+            {!loading ? (
+              itemDetailsList.map((item, index) => {
+                return (
+                  <CartList
+                    key={item.id}
+                    item={item}
+                    available={available_orders[index]}
+                  />
+                );
+              })
+            ) : (
+              <div>Loading...</div>
+            )}
+          </div>
+        </ScrollArea>
+        <div
+          className={`w-full h-full overflow-hiddens absolute transition-all duration-500 ${
+            orderConfirmed ? "left-[0%]" : "left-[100%]"
+          }`}
+        >
+          <OrderForm
+            validated_quantity={validated_quantity}
+            validated_total_cost={validated_total_cost}
+          />
         </div>
-      </ScrollArea>
+      </div>
       <hr />
       <div className="grid grid-cols-[60%_20%_20%] justify-items-center px-3">
         <span className="justify-self-end">Total</span>
@@ -60,7 +78,25 @@ export default function CartOrderSubmission({
         <span className="text-bold">₱ {validated_total_cost}</span>
       </div>
       <div className="flex justify-center">
-        <Button onClick={() => {}}>Confirm Order</Button>
+        <Button
+          onClick={() => {
+            if (!orderConfirmed) {
+              const cart = localStorage.getItem("cart");
+              if (cart) {
+                const parsedCart = JSON.parse(cart);
+                parsedCart.filter((index: number) => available_orders[index]);
+                console.log(parsedCart);
+                localStorage.setItem("cart", JSON.stringify(parsedCart));
+
+                if (parsedCart.length > 0) {
+                  setOrderConfirmed(true);
+                }
+              }
+            }
+          }}
+        >
+          {!orderConfirmed ? "Confirm Order" : "Send Order"}
+        </Button>
       </div>
     </>
   );
