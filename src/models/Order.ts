feda191a52cba2a4fd239f5d1e9f3120e20329ee
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-07-19 18:24:23 +0800

    added receiver details field

--- src/models/Order.ts
+++ src/models/Order.ts
@@ -16,6 +16,13 @@ export const OrderTypeModel = z.enum([
   "ParkNGo",
 ]);
 
+export const OrderReceiverDetailsModel = z.object({
+  name: z.string().min(1),
+  contact_number: z.string().min(1),
+  vehicle_plate: z.string().optional(),
+  vehicle_color: z.string().optional(),
+});
+
 export const OrderModel = z.object({
   id: z.string().optional(),
   created_at: z.date().optional(),
@@ -25,6 +32,7 @@ export const OrderModel = z.object({
   order_type: OrderTypeModel,
   scheduled: z.date().optional(),
   payment_pic: z.string().optional(),
+  receiver_details: OrderReceiverDetailsModel,
   mop: z.string(),
   items: CartModel,
   total_price: z.number(),
@@ -34,3 +42,4 @@ export const OrderModel = z.object({
 export type Order = z.infer<typeof OrderModel>;
 export type OrderStatus = z.infer<typeof OrderStatusModel>;
 export type OrderType = z.infer<typeof OrderTypeModel>;
+export type OrderReceiverDetails = z.infer<typeof OrderReceiverDetailsModel>;
