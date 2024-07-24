4840bde1e023663b31ebde3b95fd5f971449e8b1
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-07-24 19:13:27 +0800

    include socket config in production mode

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,8 +1,13 @@
 import { io } from "socket.io-client";
 
-const socket = io("ws://localhost:8080", {
-  autoConnect: false,
-  reconnectionAttempts: 3,
-});
+const socket = io(
+  process.env.NODE_ENV === "production"
+    ? "ws://vps-99b080e8.vps.ovh.ca:8080"
+    : "ws://localhost:8080",
+  {
+    autoConnect: false,
+    reconnectionAttempts: 3,
+  }
+);
 
 export default socket;
