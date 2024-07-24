7728d76759d31a3e33b6263187fdeffaa6b026e2
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-07-24 12:58:56 +0800

    updated user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,8 +1,29 @@
-export type UserRole = "admin" | "staff" | "customer";
-export interface User {
-  id: number;
-  email: string;
-  password: string;
-  name: string;
-  role: UserRole;
-}
+import { z } from "zod";
+
+export const StaffRoleModel = z.enum(["Admin", "Employee", "Dev"]);
+
+export const StaffModel = z.object({
+  id: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+  name: z.string(),
+  role: StaffRoleModel,
+});
+
+export const CustomerModel = z.object({
+  id: z.string(),
+  email: z.string().email(),
+  points: z.number().default(0),
+  password: z.string(),
+  name: z.string(),
+});
+
+export const LoginModel = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+export type StaffRole = z.infer<typeof StaffRoleModel>;
+export type Staff = z.infer<typeof StaffModel>;
+export type Customer = z.infer<typeof CustomerModel>;
+export type Login = z.infer<typeof LoginModel>;
