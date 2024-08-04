4be611bf77c25def27156bfe4e1197a86340d972
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-08-05 05:42:23 +0800

    added isLeapYear dependency

--- src/lib/dayjs.ts
+++ src/lib/dayjs.ts
@@ -1,8 +1,10 @@
 import dayjs from "dayjs";
 import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
+import isLeapYear from "dayjs/plugin/isLeapYear";
 
 dayjs.extend(timezone);
 dayjs.extend(utc);
+dayjs.extend(isLeapYear);
 
 export default dayjs;
