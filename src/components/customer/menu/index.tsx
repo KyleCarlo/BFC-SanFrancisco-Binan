ba8f4c5cfea49c3026020994a968e4de14f7fe4b
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-08-22 21:18:01 +0800

    added components for viewing menu

---
+++ src/components/customer/menu/index.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import { Beverage, BeverageBase } from "@models/Menu/Beverage";
+import { Food } from "@models/Menu/Food";
+import CartDialog from "@components/customer/addToCart/form-dialog";
+import { CartDrawerProvider } from "@context/cartDrawer";
+import { useOrderFilterContext } from "@context/orderFilter";
+
+import ItemCard from "@components/customer/item-card";
+import { ItemTypeProvider } from "@context/itemType";
+import { getBeveragePerBase } from "@lib/customer-utils";
+import { parseBeverageBase } from "@lib/utils";
+import { Card, CardContent, CardHeader, CardTitle } from "@components/ui/card";
+import { useEffect, useState } from "react";
+
+export default function Items({
+  beverage,
+  food,
+}: Readonly<{
+  beverage: Beverage[];
+  food: Food[];
+}>) {
+  const [beverageFiltered, setBeverageFiltered] =
+    useState<Beverage[]>(beverage);
+  const [foodFiltered, setFoodFiltered] = useState<Food[]>(food);
+  const { orderFilter } = useOrderFilterContext();
+
+  useEffect(() => {
+    if (orderFilter === undefined) {
+      setBeverageFiltered(beverage);
+      setFoodFiltered(food);
+      return;
+    }
+
+    const beverageFiltered = beverage.filter((item) =>
+      item.name.toLowerCase().includes(orderFilter.toLowerCase())
+    );
+    const foodFiltered = food.filter((item) =>
+      item.name.toLowerCase().includes(orderFilter.toLowerCase())
+    );
+
+    setBeverageFiltered(beverageFiltered);
+    setFoodFiltered(foodFiltered);
+  }, [orderFilter]);
+
+  const bevPerBase = getBeveragePerBase(beverageFiltered);
+
+  if (beverageFiltered.length === 0 && foodFiltered.length === 0) {
+    return (
+      <div className="flex items-center justify-center h-[60vh]">
+        <h1 className="text-xl text-bold text-italic text-center">
+          We cannot find what you are looking for. 😢
+        </h1>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <ItemTypeProvider defaultItemType="beverage">
+        <CartDrawerProvider>
+          {Object.keys(bevPerBase).map((base) => {
+            const parsedBase = parseBeverageBase(base);
+            if (bevPerBase[base as BeverageBase].length === 0) return null;
+
+            return (
+              <Card className="m-2 pb-4" key={base}>
+                <CardHeader className="pt-3 pb-2">
+                  <CardTitle className="text-xl text-bold text-italic tracking-wide pl-[2%]">
+                    {parsedBase}
+                  </CardTitle>
+                  <hr />
+                </CardHeader>
+                <CardContent className="p-0 grid grid-cols-[repeat(auto-fit,_minmax(150px,_1fr))] min-[400px]:grid-cols-[repeat(auto-fit,_minmax(180px,_1fr))] justify-items-center items-start">
+                  {bevPerBase[base as BeverageBase].map((item: Beverage) => {
+                    return (
+                      <CartDialog item={item} key={item.id}>
+                        <ItemCard item={item} />
+                      </CartDialog>
+                    );
+                  })}
+                </CardContent>
+              </Card>
+            );
+          })}
+        </CartDrawerProvider>
+      </ItemTypeProvider>
+      {foodFiltered.length > 0 && (
+        <ItemTypeProvider defaultItemType="food">
+          <CartDrawerProvider>
+            <Card className="m-2 pb-4">
+              <CardHeader className="pt-3 pb-2">
+                <CardTitle className="text-xl text-bold text-italic tracking-wide pl-[2%]">
+                  House Specials
+                </CardTitle>
+                <hr />
+              </CardHeader>
+              <CardContent className="p-0 grid grid-cols-[repeat(auto-fit,_minmax(150px,_1fr))] min-[400px]:grid-cols-[repeat(auto-fit,_minmax(180px,_1fr))] justify-items-center items-start">
+                {foodFiltered.map((item: Food) => {
+                  return (
+                    <CartDialog item={item} key={item.id}>
+                      <ItemCard item={item} />
+                    </CartDialog>
+                  );
+                })}
+              </CardContent>
+            </Card>
+          </CartDrawerProvider>
+        </ItemTypeProvider>
+      )}
+    </>
+  );
+}
---
+++ src/components/customer/menu/new.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { Beverage } from "@models/Menu/Beverage";
+import { Food } from "@models/Menu/Food";
+import CartDialog from "@components/customer/addToCart/form-dialog";
+import { CartDrawerProvider } from "@context/cartDrawer";
+
+import ItemCard from "@components/customer/item-card";
+import { ItemTypeProvider } from "@context/itemType";
+import { Card, CardContent, CardHeader, CardTitle } from "@components/ui/card";
+
+import { useEffect, useState } from "react";
+import { useOrderFilterContext } from "@context/orderFilter";
+
+export default function NewItems({
+  beverage,
+  food,
+}: Readonly<{
+  beverage: Beverage[];
+  food: Food[];
+}>) {
+  const [beverageFiltered, setBeverageFiltered] =
+    useState<Beverage[]>(beverage);
+  const [foodFiltered, setFoodFiltered] = useState<Food[]>(food);
+  const { orderFilter } = useOrderFilterContext();
+
+  useEffect(() => {
+    if (orderFilter === undefined) {
+      setBeverageFiltered(beverage);
+      setFoodFiltered(food);
+      return;
+    }
+
+    const beverageFiltered = beverage.filter((item) =>
+      item.name.toLowerCase().includes(orderFilter.toLowerCase())
+    );
+    const foodFiltered = food.filter((item) =>
+      item.name.toLowerCase().includes(orderFilter.toLowerCase())
+    );
+
+    setBeverageFiltered(beverageFiltered);
+    setFoodFiltered(foodFiltered);
+  }, [orderFilter]);
+  return (
+    <>
+      {(beverageFiltered.length > 0 || foodFiltered.length > 0) && (
+        <Card className="m-2 pb-4">
+          <CardHeader className="pt-3 pb-2">
+            <CardTitle className="text-xl text-bold text-italic tracking-wide pl-[2%]">
+              😳 New 😳
+            </CardTitle>
+            <hr />
+          </CardHeader>
+          <CardContent className="p-0 grid grid-cols-[repeat(auto-fit,_minmax(150px,_1fr))] min-[400px]:grid-cols-[repeat(auto-fit,_minmax(180px,_1fr))] justify-items-center items-start">
+            {beverageFiltered.map((item: Beverage) => {
+              return (
+                <ItemTypeProvider defaultItemType="beverage" key={item.id}>
+                  <CartDrawerProvider>
+                    <CartDialog item={item}>
+                      <ItemCard item={item} />
+                    </CartDialog>
+                  </CartDrawerProvider>
+                </ItemTypeProvider>
+              );
+            })}
+            {foodFiltered.map((item: Food) => {
+              return (
+                <ItemTypeProvider defaultItemType="food" key={item.id}>
+                  <CartDrawerProvider>
+                    <CartDialog item={item}>
+                      <ItemCard item={item} />
+                    </CartDialog>
+                  </CartDrawerProvider>
+                </ItemTypeProvider>
+              );
+            })}
+          </CardContent>
+        </Card>
+      )}
+    </>
+  );
+}
