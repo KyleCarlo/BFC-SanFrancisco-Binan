a45e4051458733bbb0ab89571e5b8f6cd4e1e324
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-08-23 22:25:41 +0800

    fix the long text

--- src/app/order/[id]/page.tsx
+++ src/app/order/[id]/page.tsx
@@ -27,40 +27,44 @@ export default async function WaitingPage({
   }
 
   return (
-    <div className="h-[100dvh] grid grid-rows-[0.2fr_1fr] overflow-hidden p-4 items-stretch justify-center text-center">
-      <OrderStatus
-        id={order.id as string}
-        status={order.status}
-        type={order.order_type as OrderType}
-      />
-      <Tabs
-        defaultValue="qr-code"
-        className="w-full max-w-[500px] overflow-hidden"
-      >
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="qr-code">QR Code</TabsTrigger>
-          <TabsTrigger value="order">Order Details</TabsTrigger>
-        </TabsList>
-        <TabsContent value="qr-code">
-          <div className="py-2">
-            <p>Order ID: {params.id}</p>
-            <div className="flex flex-col items-center">
-              <QRDownload value={params.id} filename={params.id} />
+    <div className="flex justify-center">
+      <div className="h-dvh grid grid-rows-[0.2fr_1fr] overflow-hidden p-4 items-stretch justify-items-center text-center absolute top-0">
+        <OrderStatus
+          id={order.id as string}
+          status={order.status}
+          type={order.order_type as OrderType}
+        />
+        <Tabs
+          defaultValue="qr-code"
+          className="w-full max-w-[500px] overflow-hidden"
+        >
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="qr-code">QR Code</TabsTrigger>
+            <TabsTrigger value="order">Order Details</TabsTrigger>
+          </TabsList>
+          <TabsContent value="qr-code">
+            <div className="py-2">
+              <p>Order ID: {params.id}</p>
+              <div className="flex flex-col items-center">
+                <QRDownload value={params.id} filename={params.id} />
+              </div>
+              <h1>
+                Keep the QR for Tracking and <br /> Receiving the Order.
+              </h1>
             </div>
-            <h1>Keep the QR for Tracking and Receiving the Order.</h1>
-          </div>
-        </TabsContent>
-        <TabsContent value="order" className="h-2/3">
-          <ScrollArea className="min-w-[300px] h-full px-4">
-            <div className="flex flex-col h-full">
-              {itemDetails.map((item) => (
-                <CartList key={item.id} item={item} available={true} />
-              ))}
-            </div>
-            <span className="text-italic">-- End Of Order --</span>
-          </ScrollArea>
-        </TabsContent>
-      </Tabs>
+          </TabsContent>
+          <TabsContent value="order" className="h-2/3">
+            <ScrollArea className="min-w-[300px] h-full px-4">
+              <div className="flex flex-col h-full">
+                {itemDetails.map((item) => (
+                  <CartList key={item.id} item={item} available={true} />
+                ))}
+              </div>
+              <p className="text-italic my-4">-- End Of Order --</p>
+            </ScrollArea>
+          </TabsContent>
+        </Tabs>
+      </div>
     </div>
   );
 }
