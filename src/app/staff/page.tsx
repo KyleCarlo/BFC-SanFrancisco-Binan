6c51b9a1e65ca95c76145f563b8ec1e66e86e835
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-08-26 12:32:37 +0800

    added logout for staff

--- src/app/staff/page.tsx
+++ src/app/staff/page.tsx
@@ -5,19 +5,56 @@ import getStaffs from "@hooks/getStaffs";
 import { useEffect, useState } from "react";
 import DataTable from "@components/ui/data-table";
 import staffColumns from "@components/staff/staffTable/columns";
+import { Button } from "@components/ui/button";
+import { getSession, logout } from "@lib/auth";
+import { UserSession } from "@models/User";
+import { redirect } from "next/navigation";
 
 export default function StaffHomePage() {
   const [loading, setLoading] = useState(true);
+  const [loadingUser, setLoadingUser] = useState(true);
   const [staffs, setStaffs] = useState<Staff[]>([]);
+  const [user, setUser] = useState<UserSession | null>(null);
 
   useEffect(() => {
     getStaffs(setStaffs, setLoading);
   }, []);
 
+  useEffect(() => {
+    async function getUser() {
+      setLoadingUser(true);
+      const { session } = (await getSession()) as {
+        session: { user: UserSession };
+      };
+
+      if (session && session.user.role !== "Customer") {
+        setUser(session.user);
+      }
+      setLoadingUser(false);
+    }
+
+    getUser();
+  }, []);
+
   return (
-    <div className="p-4">
-      {loading && <span>Loading...</span>}
-      {!loading && <DataTable columns={staffColumns} data={staffs} />}
-    </div>
+    <main className="space-y-2 p-4">
+      <nav className="flex justify-between">
+        <p className="border-2 rounded-md flex items-center px-4">
+          Logged in as: {user?.first_name}
+        </p>
+        <Button
+          onClick={async () => {
+            await logout();
+            redirect("/staff/sign-in");
+          }}
+        >
+          Logout
+        </Button>
+      </nav>
+      <div>
+        {loading && <span>Loading...</span>}
+        {!loading && <DataTable columns={staffColumns} data={staffs} />}
+      </div>
+    </main>
   );
 }
