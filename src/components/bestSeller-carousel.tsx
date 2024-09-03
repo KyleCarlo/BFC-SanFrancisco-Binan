3a2b431c10309122438c320bdd227907e69b6b94
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-09-03 20:14:41 +0800

    fixed carousel

--- src/components/bestSeller-carousel.tsx
+++ src/components/bestSeller-carousel.tsx
@@ -17,7 +17,7 @@ export default function BestSellers() {
 
   return (
     <Carousel
-      className="relative max-[350px]:w-[200px] max-[400px]:w-[300px] max-[460px]:w-[360px] max-[715px]:w-[400px] min-[715px]:mx-10"
+      className="mx-2 w-[200px] min-[350px]:w-[calc(100dvw-90px)]"
       opts={{
         align: "start",
         loop: true,
@@ -30,8 +30,8 @@ export default function BestSellers() {
             key={item.id}
             className="min-[350px]:basis-1/2 md:basis-1/3 lg:basis-1/4 flex justify-center"
           >
-            <div className="w-full min-[400px]:max-w-[200px] min-[715px]:max-w-72">
-              <div className="min-[400px]:w-full relative pt-[100%]">
+            <div className="w-full max-w-72">
+              <div className="w-full relative pt-[100%]">
                 <Image
                   fill={true}
                   src={`/best_sellers/${item.image}`}
@@ -39,15 +39,15 @@ export default function BestSellers() {
                   className="w-full h-full top-0 left-0 object-cover rounded-md"
                 />
               </div>
-              <h1 className="text-gold text-bold text-italic px-2 pt-1 pb-2 text-center text-2xl text-italic max-[900px]:text-sm">
+              <h1 className="text-gold text-bold text-italic px-2 pt-1 pb-2 text-center text-2xl text-italic max-[1170px]:text-xl max-[900px]:text-sm">
                 {item.name}
               </h1>
             </div>
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious className="top-[calc(50%-15px)] max-[540px]:left-[-20px]" />
-      <CarouselNext className="top-[calc(50%-15px)] max-[540px]:right-[-20px]" />
+      <CarouselPrevious className="top-[calc(50%-15px)] left-[-20px]" />
+      <CarouselNext className="top-[calc(50%-15px)] right-[-20px]" />
     </Carousel>
   );
 }
