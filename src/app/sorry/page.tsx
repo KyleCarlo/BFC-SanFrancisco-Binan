8ac8270c705339f6aadaa7aad848d9d36cbb15d3
Kyle Carlo Lasala <kyle_lasala@dlsu.edu.ph>
2024-12-12 22:15:31 +0800

    temporary disabled

---
+++ src/app/sorry/page.tsx
@@ -0,0 +1,12 @@
+export default function SorryPage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen p-4">
+      <h1 className="text-5xl font-bold">Sorry for the inconvenience 😔</h1>
+      <p className="text-lg mt-3 text-justify">
+        Apologies, our ordering side is still under maintenance. We aim to
+        improve our service and look forward to offer our Improved Park & Go
+        service soon. Thank you.
+      </p>
+    </div>
+  );
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,82 +5,84 @@ import { UserSession } from "@models/User";
 
 export async function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
-  const session = request.cookies.get("bfc-sfb-session");
-
-  if (path.startsWith("/staff") && path !== "/staff/sign-in") {
-    if (!session) {
-      return NextResponse.redirect(new URL("/staff/sign-in", request.url));
-    }
-    const { user } = (await decrypt(session.value)) as { user: UserSession };
-    if (!Object.keys(StaffRoleModel.Values).includes(user.role))
-      return NextResponse.redirect(new URL(`/account/${user.id}`, request.url));
-  } else if (path === "/staff/sign-in" && session) {
-    const { user } = (await decrypt(session.value)) as { user: UserSession };
-    if (!Object.keys(StaffRoleModel.Values).includes(user.role))
-      return NextResponse.redirect(new URL(`/account/${user.id}`, request.url));
-    else return NextResponse.redirect(new URL("/staff", request.url));
+  // const session = request.cookies.get("bfc-sfb-session");
+  if (path != "/" && path != "/sorry") {
+    return NextResponse.redirect(new URL("/sorry", request.url));
   }
-  if (path === "/api/menu" && request.method != "GET") {
-    if (!session) {
-      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-    }
-    const { user } = (await decrypt(session.value)) as { user: UserSession };
-    if (
-      !["Admin", "Dev", "Employee"].includes(user.role) &&
-      request.method === "PATCH"
-    ) {
-      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-    }
-    if (!["Admin", "Dev"].includes(user.role) && request.method !== "PATCH") {
-      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-    }
-  }
-  if (path.startsWith("/account")) {
-    if (!session)
-      return NextResponse.redirect(new URL("/sign-in", request.url));
+  // if (path.startsWith("/staff") && path !== "/staff/sign-in") {
+  //   if (!session) {
+  //     return NextResponse.redirect(new URL("/staff/sign-in", request.url));
+  //   }
+  //   const { user } = (await decrypt(session.value)) as { user: UserSession };
+  //   if (!Object.keys(StaffRoleModel.Values).includes(user.role))
+  //     return NextResponse.redirect(new URL(`/account/${user.id}`, request.url));
+  // } else if (path === "/staff/sign-in" && session) {
+  //   const { user } = (await decrypt(session.value)) as { user: UserSession };
+  //   if (!Object.keys(StaffRoleModel.Values).includes(user.role))
+  //     return NextResponse.redirect(new URL(`/account/${user.id}`, request.url));
+  //   else return NextResponse.redirect(new URL("/staff", request.url));
+  // }
+  // if (path === "/api/menu" && request.method != "GET") {
+  //   if (!session) {
+  //     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  //   }
+  //   const { user } = (await decrypt(session.value)) as { user: UserSession };
+  //   if (
+  //     !["Admin", "Dev", "Employee"].includes(user.role) &&
+  //     request.method === "PATCH"
+  //   ) {
+  //     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  //   }
+  //   if (!["Admin", "Dev"].includes(user.role) && request.method !== "PATCH") {
+  //     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  //   }
+  // }
+  // if (path.startsWith("/account")) {
+  //   if (!session)
+  //     return NextResponse.redirect(new URL("/sign-in", request.url));
 
-    const { user } = (await decrypt(session.value)) as { user: UserSession };
-    if (user.role === "Customer" && path !== `/account/${user.id}`)
-      return NextResponse.redirect(new URL(`/account/${user.id}`, request.url));
+  //   const { user } = (await decrypt(session.value)) as { user: UserSession };
+  //   if (user.role === "Customer" && path !== `/account/${user.id}`)
+  //     return NextResponse.redirect(new URL(`/account/${user.id}`, request.url));
 
-    if (["Admin", "Employee"].includes(user.role))
-      return NextResponse.redirect(new URL("/staff", request.url));
-  }
+  //   if (["Admin", "Employee"].includes(user.role))
+  //     return NextResponse.redirect(new URL("/staff", request.url));
+  // }
 
-  if (
-    ["/api/customer", "/api/customer/voucher"].includes(path) &&
-    request.method === "GET"
-  ) {
-    const id = request.nextUrl.searchParams.get("id");
-    if (!id) {
-      return NextResponse.json(
-        { message: "Invalid Customer ID." },
-        { status: 400 }
-      );
-    }
-    if (!session) {
-      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-    }
-    const { user } = (await decrypt(session.value)) as { user: UserSession };
-    if (user.role === "Customer" && id !== user.id) {
-      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-    }
-    if (["Admin", "Employee"].includes(user.role))
-      return NextResponse.redirect(new URL("/staff", request.url));
-  }
+  // if (
+  //   ["/api/customer", "/api/customer/voucher"].includes(path) &&
+  //   request.method === "GET"
+  // ) {
+  //   const id = request.nextUrl.searchParams.get("id");
+  //   if (!id) {
+  //     return NextResponse.json(
+  //       { message: "Invalid Customer ID." },
+  //       { status: 400 }
+  //     );
+  //   }
+  //   if (!session) {
+  //     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  //   }
+  //   const { user } = (await decrypt(session.value)) as { user: UserSession };
+  //   if (user.role === "Customer" && id !== user.id) {
+  //     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  //   }
+  //   if (["Admin", "Employee"].includes(user.role))
+  //     return NextResponse.redirect(new URL("/staff", request.url));
+  // }
 
-  if (
-    (path === "/api/customer" && request.method === "PATCH") ||
-    (path === "/api/customer/voucher" && request.method === "DELETE")
-  ) {
-    if (!session) {
-      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-    }
-    const { user } = (await decrypt(session.value)) as { user: UserSession };
-    if (!["Admin", "Employee", "Dev"].includes(user.role)) {
-      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-    }
-  }
+  // if (
+  //   (path === "/api/customer" && request.method === "PATCH") ||
+  //   (path === "/api/customer/voucher" && request.method === "DELETE")
+  // ) {
+  //   if (!session) {
+  //     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  //   }
+  //   const { user } = (await decrypt(session.value)) as { user: UserSession };
+  //   if (!["Admin", "Employee", "Dev"].includes(user.role)) {
+  //     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  //   }
+  // }
   return NextResponse.next();
 }
 
